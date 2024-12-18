a1d6d920f1538579318544de8d19e2b5608c07dd
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-12-18 16:13:30 +0900

    Title: 코드트리 메신저, Time: 18ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241218/코드트리 메신저/codetree-messenger.cpp
+++ 241218/코드트리 메신저/codetree-messenger.cpp
@@ -2,15 +2,16 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+using vi = vector<int>;
 const int MAX = 100001;
 const int MAX_POWER = 20;
 
 int N, Q;
 int parents[MAX];
-vector<vector<int>> children(MAX);
 int auth[MAX];
-vector<vector<int>> authDP(MAX, vector<int>(21, 0));
-vector<bool> alarms(MAX, true); // 전체를 true로 초기화
+bool alarms[21] = {true, }; // true로 초기화
+vector<vi> children(MAX);
+vector<vi> alarmDP(MAX, vi(21, 0));
 
 void init() {
     for (int i = 1; i <= N; ++i) {
@@ -26,7 +27,7 @@ void init() {
         children[parents[i]].push_back(i);
         int power = auth[i], idx = i;
         while (power >= 0) {
-            authDP[idx][power] += 1;
+            alarmDP[idx][power] += 1;
             power--;
 
             if (idx == parents[idx]) break;
@@ -36,34 +37,34 @@ void init() {
 }
 
 void update(int idx) {
-    fill(authDP[idx].begin(), authDP[idx].end(), 0);
-    authDP[idx][auth[idx]] += 1;
+    fill(alarmDP[idx].begin(), alarmDP[idx].end(), 0);
+    alarmDP[idx][auth[idx]] += 1;
 
-    // 자식 노드들의 정보 합산 (인덱스 범위 주의)
+    // 자식 노드들 range 합산
     for (int child : children[idx]) {
         if (!alarms[child]) continue;
         for (int i = 0; i < MAX_POWER; ++i) {
-            authDP[idx][i] += authDP[child][i+1];
+            alarmDP[idx][i] += alarmDP[child][i+1];
         }
     }
 
-    // 부모로 값 전파
+    // 부모 값 update, root 갈 때까지
     if (idx != parents[idx]) {
         update(parents[idx]);
     }
 }
 
-void change_alarm(int c) {
-    alarms[c] = !alarms[c];  // 알람 상태 변경
-    update(parents[c]);  // 부모로 값 전파
+void changeAlarm(int c) {
+    alarms[c] = !alarms[c];
+    update(parents[c]);
 }
 
-void change_power(int c, int power) {
-    auth[c] = min(MAX_POWER, power);  // 권한 변경
-    update(c);  // 해당 노드부터 갱신
+void changePower(int c, int power) {
+    auth[c] = min(MAX_POWER, power);
+    update(c);
 }
 
-void change_parents(int c1, int c2) {
+void changeParents(int c1, int c2) {
     int p1 = parents[c1], p2 = parents[c2];
     if (p1 == p2) return;
 
@@ -71,7 +72,7 @@ void change_parents(int c1, int c2) {
     parents[c1] = p2;
     parents[c2] = p1;
 
-    // 자식 관계를 서로 바꿔준다.
+    // 자식 관계 스왑
     auto it1 = find(children[p1].begin(), children[p1].end(), c1);
     if (it1 != children[p1].end()) children[p1].erase(it1);
     children[p2].push_back(c1);
@@ -79,16 +80,15 @@ void change_parents(int c1, int c2) {
     auto it2 = find(children[p2].begin(), children[p2].end(), c2);
     if (it2 != children[p2].end()) children[p2].erase(it2);
     children[p1].push_back(c2);
-
-    // 갱신
+    
     update(p1);
     update(p2);
 }
 
-void check(int cur) {
+void printRet(int cur) {
     int ret = 0;
     for (int i = 0; i < 21; ++i) {
-        ret += authDP[cur][i];
+        ret += alarmDP[cur][i];
     }
     cout << ret-1 << "\n";
 }
@@ -103,18 +103,17 @@ int main() {
         if(comm==100) init();
         else if (comm == 200) {
             cin >> c1;
-            change_alarm(c1);
+            changeAlarm(c1);
         } else if (comm == 300) {
             cin >> c1 >> c2;
-            change_power(c1, c2);
+            changePower(c1, c2);
         } else if (comm == 400) {
             cin >> c1 >> c2;
-            change_parents(c1, c2);
+            changeParents(c1, c2);
         } else if (comm == 500) {
             cin >> c1;
-            check(c1);
+            printRet(c1);
         }
     }
-
     return 0;
 }
