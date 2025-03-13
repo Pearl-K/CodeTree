021ac13ed9f9a17752715311dc048d06e040d6d9
Pearl <80253713+Pearl-K@users.noreply.github.com>
2025-03-13 21:53:34 +0900

    Title: G & H 반전시키기, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250313/G & H 반전시키기/reversing-g-and-h.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+a = input()
+b = input()
+
+res = 0
+flg = False
+
+for i in range(N):
+    if a[i] != b[i]:
+        if not flg:
+            flg = True
+            res += 1
+        else:
+            flg = False
+print(res)
